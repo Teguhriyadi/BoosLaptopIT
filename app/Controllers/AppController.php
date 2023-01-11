f058c2d70ff5e3d1bee5568e6094838fd29b5142
Teguhriyadi <ilham.teguh55@gmail.com>
2023-01-11 20:52:47 +0700

    Update Dashboard

--- app/Controllers/AppController.php
+++ app/Controllers/AppController.php
@@ -6,6 +6,13 @@ use App\Controllers\BaseController;
 
 class AppController extends BaseController
 {
+    protected $session;
+
+    public function __construct()
+    {
+        $this->session = session();    
+    }
+
     public function index()
     {
         return view("dashboard");
--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -9,14 +9,27 @@ Dashboard
 <?= $this->section("content"); ?>
 
 <ol class="breadcrumb">
-    <li class="breadcrumb-item">
-        <a href="index.html">Dashboard</a>
+    <li class="breadcrumb-item active">
+        Dashboard
     </li>
-    <li class="breadcrumb-item active">Blank Page</li>
 </ol>
 <div class="row">
     <div class="col-12">
-        <h1>Blank</h1>
+        <div class="alert alert-success">
+            <?php 
+                $session = session();
+            ?>
+            Selamat Datang 
+            <strong>
+                <?= $session->get("username") ?>
+            </strong> 
+                di Aplikasi 
+            <strong>
+                BoosLaptopIT.
+            </strong> 
+            Silahkan Pilih Menu Untuk Memulai Program.
+        </div>
+        <h1></h1>
         <p>This is an example of a blank page that you can use as a starting point for creating new ones.</p>
     </div>
 </div>
